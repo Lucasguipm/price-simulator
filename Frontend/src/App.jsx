9d7c4f442b4384329b3234110a106bbf5cb3f98c
Lucas Guilherme <lucasguilhermepereiramoura@gmail.com>
2026-07-27 20:51:53 -0300

    fix: App.jsx route update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     setError('');
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/analyze', { url });
+      const response = await axios.post('https://price-simulator-lgpm.vercel.app/api/analyze', { url });
       setProductData(response.data);
     } catch (err) {
       console.error(err);
